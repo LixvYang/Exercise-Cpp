f7d8d81709efaa47c5fd7347bab1c2bd118f6335
lixin yang <2690688423@qq.com>
2022-06-07 18:05:01 +0800

    update

---
+++ 02/alias.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+void main() {
+  typedef double wages;
+
+  using SI = Sales_item;
+  wages hourly, weekly;
+  SI item;
+
+  type char *pstring;
+  const pstring cstr = 0;
+  const pstring *ps;
+
+  const char *cstr = 0;
+
+  auto item = val1 + val2;
+  auto i = 0, *p = &i;
+  auto sz = 0, pi = 3.14 // 错误
+
+  decltype(f()) sum = x;
+
+
+}
---
+++ 02/class.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+// struct Sales_data { /** */ } accum, trans, *salesptr;
+// //
+// struct Sales_data {};
+// Sales_data accum, trans, *salesptr;
+
+// struct Foo {}
+// struct Sales_data
+// {
+//   std::string bookNo;
+//   std::string bookName;
+//   unsigned units_sold = 0;
+//   double revenue = 0.0;
+//   double price = 0.0;
+// }
+
+int main() {
+  return 0;
+}
