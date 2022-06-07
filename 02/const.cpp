eb31d8ba4105c5ccd39e44b73fe673d6e406c850
lixin yang <2690688423@qq.com>
2022-06-07 16:55:58 +0800

    update

--- 02/const.cpp
+++ 02/const.cpp
@@ -1,13 +1,44 @@
 #include<iostream>
+using namespace std;
 
 int main() {
   // extern const int bufSize = 10;
   // bufSize = 512;
   // std::cout << bufSize << std::endl;
 
-  const int ci = 42;
-  const int &ri = ci;
+  // const int ci = 42;
+  // const int &ri = ci;
+  // cout << ri << endl;
+  // const int ci = 1024;
+  // const int &r1 = ci;
+  // cout << r1 << endl;
+  // cout << &ci << endl;
+  // r1 = 42;
+  // int &r2 = ci;
 
-  int &r2 = ci;
+  /**
+   * @brief 常量的临时拷贝
+   */
+  // int i = 42;
+  // const int &r1 = i;
+  // const int &r2 = 42;
+  // const int &r3 = r1*2;
+  // int &r4 = i;
+  // double dval = 3.14;
+  // const int &ri = dval;
+  // cout << ri << endl;
 
+  // const 指针
+  // int errNumb = 0;
+  // int *const curErr = &errNumb;
+  // const double pi = 3.1415926;
+  // const double *const pip = &pi;
+
+// const int i = -1, &r = 0;
+  // int i, *const cp;
+  // int *pl, *const p2;
+  int i2 = 2;          // // 底层const i是一个常量
+  const int *const p3 = &i2;  
+  i2 = 3;
+  cout << *p3 << endl;
 }
---
+++ 02/constexpr.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+void main() {
+  // 是否是常量表达式
+  const int max_files = 20;
+  const int limit = max_files + 1;
+  int staff_size = 27;
+  const int sz = get_size()
+
+  // constexpr变量
+  constexpr int mf = 20;
+  constexpr int limit = mf+1;
+  constexpr int sz = get_size();
+
+  const int *p = nullptr;
+  constexpr int *1 = nullptr;
+
+}
