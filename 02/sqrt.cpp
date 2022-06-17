7b003621f505f1ac7d3a434eba090a88d7cc5151
lixin yang <2690688423@qq.com>
2022-06-17 15:27:50 +0800

    update

---
+++ 02/sqrt.cpp
@@ -0,0 +1,17 @@
+// sqrt
+//
+#include <iostream>
+#include <cmath>
+
+int main() 
+{
+  using namespace std;
+
+  double area;
+
+  cin >> area;
+  double side;
+  side = sqrt(area);
+  cout << side << endl;
+  return 0;
+}
