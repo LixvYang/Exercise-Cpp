bffddee39558a04d21251f9ab6e07ac45254f9aa
lixin yang <2690688423@qq.com>
2022-06-26 21:47:06 +0800

    update

--- manager/通讯录管理系统.cpp
+++ manager/通讯录管理系统.cpp
@@ -1,5 +1,60 @@
 #include <iostream>
+#include <string>
 using namespace std;
+#define MAX 2000
+
+struct Person
+{
+  string m_Name;
+  int m_Sex;
+  int m_Age;
+  string m_Phone;
+  string m_Address;
+};
+
+struct Addressbooks
+{
+  struct Person personArray[MAX];
+  int m_Size;
+};
+
+void addPerson(Addressbooks * abs) {
+  if (abs->m_Size == MAX) {
+    cout << "通讯录满了" << endl;
+    return ;
+  }
+  else 
+  {
+    string name;
+    cout << "请输入姓名: " << endl;
+    cin >> name;
+    abs->personArray[abs->m_Size].m_Name = name;
+
+    cout << "请输入性别： " << endl;
+    cout << "1 男 2 女" << endl;
+    int sex = 0;
+
+    while (true)
+    {
+      if (sex == 1 || sex == 2) {
+        abs->personArray[abs->m_Size].m_Sex = sex;
+        break;
+      }
+    }
+     
+  }
+}
+int slowIndex = 0;
+for (int fastIndex = 0; fastIndex < nums.size(); fastIndex++) {
+    if (val != nums[fastIndex]) {
+        nums[slowIndex++] = nums[fastIndex];
+    }
+}
+return slowIndex;
+
+
+
+
 
 void showMenu() 
 {
@@ -15,6 +70,9 @@ void showMenu()
 int main()
 {
   int select = 0;
+  Addressbooks abs;
+  abs.m_Size = 0;
+
   
   while (true)
   {
@@ -31,6 +89,7 @@ int main()
     //  0. 退出通讯录
     {
     case 1: 
+      addPerson(&abs);
       break;
     case 2:
       break;
